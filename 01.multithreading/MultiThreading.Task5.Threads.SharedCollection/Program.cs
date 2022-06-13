9ae01481f4d24fe1f022bcd8819493d2229ffc90
ChristinaGolovach <christina.golovach@gmail.com>
2022-06-13 11:25:07 +0200

    add task_3, task_4, task_5, task_6

--- 01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ 01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,21 +5,63 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
-        static void Main(string[] args)
+		private static int elementCount = 10;
+        private static List<int> sharedCollection = new List<int>();
+		private static object locker = new object();
+		private static AutoResetEvent readEvent = new AutoResetEvent(false);
+		private static AutoResetEvent writeEvent = new AutoResetEvent(false);
+
+		static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
             Console.WriteLine("the first one should add 10 elements into the collection and the second should print all elements in the collection after each adding.");
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
+			// feel free to add your code
+			Task addItems = Task.Run(() => AddItems());
 
-            Console.ReadLine();
+			Console.ReadLine();
         }
-    }
+
+		private static void AddItems()
+		{
+			for (int i = 0; i <= elementCount; i++)
+			{
+				Task printItems = Task.Run(() => PrintItems());
+
+				readEvent.WaitOne();
+
+				lock (locker)
+				{
+					sharedCollection.Add(i);
+				}
+				Console.WriteLine($"item {i} has been added.");
+				writeEvent.Set();
+			}
+		}
+
+		private static void PrintItems()
+		{
+			readEvent.Set();
+			writeEvent.WaitOne();
+
+			lock (locker)
+			{
+				foreach (var item in sharedCollection)
+				{
+					Console.Write($"{item} ");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
 }
--- 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
+++ 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
@@ -7,24 +7,96 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Create a Task and attach continuations to it according to the following criteria:");
-            Console.WriteLine("a.    Continuation task should be executed regardless of the result of the parent task.");
-            Console.WriteLine("b.    Continuation task should be executed when the parent task finished without success.");
-            Console.WriteLine("c.    Continuation task should be executed when the parent task would be finished with fail and parent task thread should be reused for continuation.");
-            Console.WriteLine("d.    Continuation task should be executed outside of the thread pool when the parent task would be cancelled.");
-            Console.WriteLine("Demonstrate the work of the each case with console utility.");
-            Console.WriteLine();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Create a Task and attach continuations to it according to the following criteria:");
+			Console.WriteLine("a.    Continuation task should be executed regardless of the result of the parent task.");
+			Console.WriteLine("b.    Continuation task should be executed when the parent task finished without success.");
+			Console.WriteLine("c.    Continuation task should be executed when the parent task would be finished with fail and parent task thread should be reused for continuation.");
+			Console.WriteLine("d.    Continuation task should be executed outside of the thread pool when the parent task would be cancelled.");
+			Console.WriteLine("Demonstrate the work of the each case with console utility.");
+			Console.WriteLine();
 
-            // feel free to add your code
+			// feel free to add your code
+			Task taskA = RunA();
+			Task taskB = RunB();
+			Task taskC = RunC();
+			Task taskD = RunD();
 
-            Console.ReadLine();
-        }
-    }
+			Task.WaitAll(taskA, taskB, taskC, taskD);
+
+			Console.ReadLine();
+		}
+
+		private static Task RunA()
+		{
+			Task mainTask = Task.Run(() => Console.WriteLine($"Parent Task A is runned;"));
+			return mainTask.ContinueWith(result => Console.WriteLine($"Child Task A is runned;"));
+		}
+
+		private static Task RunB()
+		{
+			var mainTask = GetTaskFromException();
+			return mainTask.ContinueWith(result => Console.WriteLine($"Child Task B is runned. Parent task is faulted - {result.IsFaulted};"),
+				TaskContinuationOptions.OnlyOnFaulted);
+		}
+
+		private static Task RunC()
+		{
+			return Task.Factory.StartNew(CompositeTaskSynchronously);
+		}
+
+		private static Task RunD()
+		{
+			using (CancellationTokenSource src = new CancellationTokenSource())
+			{
+				var token = src.Token;
+				src.Cancel();
+
+				return ParentTask(token).ContinueWith(ChildTask, TaskContinuationOptions.OnlyOnCanceled);
+			}
+		}
+
+		private static Task GetTaskFromException()
+		{
+			return Task.FromException(new ArgumentException("Invalid ***."));
+		}
+
+		private static Task GetTaskFromExceptionWithSameThread()
+		{
+			Console.WriteLine($"Parent task thread Id - {Thread.CurrentThread.ManagedThreadId}.");
+			return Task.FromException(new ArgumentException("Invalid ***."));
+		}
+
+		private static void CompositeTaskSynchronously()
+		{
+			Console.WriteLine();
+			var parentTask = GetTaskFromExceptionWithSameThread();
+			if (parentTask.IsFaulted)
+			{
+				parentTask.ContinueWith(result => Console.WriteLine($"Child Task C is runned. Parent task is faulted - {result.IsFaulted}; Current thread id - {Thread.CurrentThread.ManagedThreadId}"),
+				TaskContinuationOptions.ExecuteSynchronously);
+			}
+		}
+
+		private static Task ParentTask(CancellationToken cancellationToken)
+		{
+			return Task.FromCanceled(cancellationToken);
+		}
+
+		private static Task ChildTask(Task parentTask)
+		{
+			return parentTask.ContinueWith(task =>
+					Console.WriteLine(
+						$"Inner {nameof(ChildTask)} D is in thread pool: {Thread.CurrentThread.IsThreadPoolThread}."),
+				TaskContinuationOptions.LongRunning);
+		}
+	}
 }

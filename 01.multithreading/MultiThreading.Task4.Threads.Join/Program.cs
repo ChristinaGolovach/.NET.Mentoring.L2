df6c9bc998ff29f4baf96623d4bbcb51f0592e12
ChristinaGolovach <christina.golovach@gmail.com>
2022-06-21 07:59:13 +0200

    add initial task 2

--- 01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
+++ 01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
@@ -14,41 +14,41 @@ using System.Threading;
 
 namespace MultiThreading.Task4.Threads.Join
 {
-    class Program
-    {
-        private const int ThreadAmount = 10;
-        private static Semaphore semaphore = new Semaphore(1, 1);
+	class Program
+	{
+		private const int ThreadAmount = 10;
+		private static Semaphore semaphore = new Semaphore(1, 1);
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
-            Console.WriteLine("Each thread should be with the same body and receive a state with integer number, decrement it, print and pass as a state into the newly created thread.");
-            Console.WriteLine("Implement all of the following options:");
-            Console.WriteLine();
-            Console.WriteLine("- a) Use Thread class for this task and Join for waiting threads.");
-            Console.WriteLine("- b) ThreadPool class for this task and Semaphore for waiting threads.");
-
-            Console.WriteLine();
-
-            PrintNumbersThroughThreads(ThreadAmount, 0);
-            PrintNumbersThroughThreadPool(ThreadAmount, 0);
-
-            Console.ReadLine();
-        }
-
-        private static void PrintNumbersThroughThreads(int threadAmount, int currentNumber)
+		static void Main(string[] args)
 		{
-            if (threadAmount == 0)
+			Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
+			Console.WriteLine("Each thread should be with the same body and receive a state with integer number, decrement it, print and pass as a state into the newly created thread.");
+			Console.WriteLine("Implement all of the following options:");
+			Console.WriteLine();
+			Console.WriteLine("- a) Use Thread class for this task and Join for waiting threads.");
+			Console.WriteLine("- b) ThreadPool class for this task and Semaphore for waiting threads.");
+
+			Console.WriteLine();
+
+			PrintNumbersThroughThreads(ThreadAmount, 0);
+			PrintNumbersThroughThreadPool(ThreadAmount, 0);
+
+			Console.ReadLine();
+		}
+
+		private static void PrintNumbersThroughThreads(int threadAmount, int currentNumber)
+		{
+			if (threadAmount == 0)
 			{
-                return;
+				return;
 			}
 
-            int result = 0;
-            var thread = new Thread(() => result = PrintNextNumber(currentNumber));
-            thread.Start();
-            thread.Join();
+			int result = 0;
+			var thread = new Thread(() => result = PrintNextNumber(currentNumber));
+			thread.Start();
+			thread.Join();
 
-            PrintNumbersThroughThreads(--threadAmount, result);
+			PrintNumbersThroughThreads(--threadAmount, result);
 		}
 
 		private static void PrintNumbersThroughThreadPool(int threadAmount, int currentNumber)
@@ -58,8 +58,8 @@ namespace MultiThreading.Task4.Threads.Join
 				return;
 			}
 
-            ThreadPool.QueueUserWorkItem((i) =>
-            {
+			ThreadPool.QueueUserWorkItem((i) =>
+			{
 				semaphore.WaitOne();
 
 				currentNumber += 1;
@@ -69,14 +69,14 @@ namespace MultiThreading.Task4.Threads.Join
 
 				semaphore.Release();
 			});
-			
+
 		}
 
 		private static int PrintNextNumber(int currentNumber)
 		{
-            int nextNumber = ++currentNumber;
-            Console.WriteLine(nextNumber);
-            return nextNumber;
+			int nextNumber = ++currentNumber;
+			Console.WriteLine(nextNumber);
+			return nextNumber;
 		}
-    }
+	}
 }
